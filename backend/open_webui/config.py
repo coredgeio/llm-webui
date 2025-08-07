29d5ebb46714057f06859cad4718296a8c377cf5
Ashok Kumar <ashok@coredge.io>
2025-08-07 15:07:56 +0000

    disable url checking for openwebui fir poc

--- backend/open_webui/config.py
+++ backend/open_webui/config.py
@@ -742,7 +742,7 @@ if frontend_loader.exists():
 
 CUSTOM_NAME = os.environ.get("CUSTOM_NAME", "")
 
-if CUSTOM_NAME:
+if not CUSTOM_NAME:
     try:
         r = requests.get(f"https://api.openwebui.com/api/v1/custom/{CUSTOM_NAME}")
         data = r.json()
